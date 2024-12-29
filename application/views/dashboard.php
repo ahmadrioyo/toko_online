20351660ec611a34d27f4626fc4646b386fb10d7
ahmadrioyo <ahmadrio919@gmail.com>
2024-12-29 22:34:15 +0700

    update fitur tambah keranjang

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -22,15 +22,15 @@
             <span class="sr-only">Next</span>
         </button>
     </div>
-    <div class="row text-center mt-5">
+    <div class="row text-center mt-5" style="justify-content: center;">
         <?php foreach ($barang as $brg) : ?>
-            <div class="card ml-3 mb-3" style="width: 16rem;">
+            <div class="card ml-3 mb-3" style="width: 18rem;">
                 <img src="<?php echo base_url() . '/uploads/' . $brg->gambar ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
                     <small><?php echo $brg->keterangan ?></small><br>
-                    <span class="badge badge-success mb-3"><?php echo $brg->harga ?></span>
-                    <a href="#" class="btn btn-sm btn-primary">Tambahkan Kekeranjang</a>
+                    <span class="badge badge-success mb-3"><?php echo $brg->harga ?></span><br>
+                    <?php echo anchor('dashboard/tambah_ke_keranjang/'. $brg->id_brg, '<div class="btn btn-sm btn-primary">Tambahkan Kekeranjang</div>') ?>
                     <a href="#" class="btn btn-sm btn-success">Detail</a>
                 </div>
             </div>
